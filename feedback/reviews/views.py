70167d6fd8d49ccfaac4dc4edaa207fb7e0ca3cb
Mark <samantasudipta301@gmail.com>
2024-01-27 22:53:18 +0530

    forms second commit

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -6,9 +6,15 @@ from django.urls import reverse
 def index(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
+        if entered_username == '' or len(entered_username) >=100:
+            return render(request,'reviews/review.html',{
+                "has_error":True
+            })
         print(entered_username)
         return HttpResponseRedirect(reverse('thank-you'))
-    return render(request,'reviews/review.html')
+    return render(request,'reviews/review.html',{
+        "has_error":False
+    })
 
 def thank_you(request):
     return render(request,'reviews/thank_you.html')
