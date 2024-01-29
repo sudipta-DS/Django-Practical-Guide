1959ca31eb333ac60d757f9f51ba46ddeb5f52de
Mark <samantasudipta301@gmail.com>
2024-01-29 15:41:37 +0530

    File Uploads

--- feedback/profiles/urls.py
+++ feedback/profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateProfileView.as_view())
+    path("", views.ProfilesCreateView.as_view()),
+    path("all-profiles",view=views.ProfileListView.as_view())
 ]
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from msilib.schema import ListView
+from django.shortcuts import redirect, render
 from django.views import View
-
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from .forms import ProfileForm
+from .models import ProfilesModel
 # Create your views here.
 
+class ProfilesCreateView(CreateView):
+    model = ProfilesModel
+    fields = '__all__'
+    template_name = "profiles/create_profile.html"
+    success_url = "/profiles"
 
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, "profiles/create_profile.html")
+class ProfileListView(ListView):
+    template_name = 'profiles/user-profiles.html'
+    model = ProfilesModel
+    context_object_name = 'profiles'
 
-    def post(self, request):
-        pass
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html",{
+#             "form" : form
+#         })
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST,request.FILES)
+#         if submitted_form.is_valid():
+#             profile_image = ProfilesModel(image=request.FILES['user_image'])
+#             profile_image.save()
+#             # print(img)
+#             return render(request, "profiles/create_profile.html",{
+#                 "form" : submitted_form
+#             })
