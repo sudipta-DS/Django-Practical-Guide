d3fd5d8b47b6a87d4da03a05196866b934f23517
Mark <samantasudipta301@gmail.com>
2024-01-30 18:59:02 +0530

    1st commit on 30th January

--- blogs/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from . import views
-
-
-urlpatterns = [
-    path('',view=views.starting_page,name='starting-page'),
-    path('posts',view=views.all_post,name='posts-page'),
-    path('posts/<slug:slug>',view=views.post_details,name='post-detail-page')
-]
